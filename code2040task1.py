6159ad4c39116af597dcbe41196b74ca1e4fb0dc
DalidaMartinez <dalidamartinez@gmail.com>
2016-11-01 13:24:41 -0400

    task #1 completed

--- code2040task1.py
+++ code2040task1.py
@@ -1,15 +1,20 @@
-from urllib2 import Request,urlopen,URLError
+import requests
 import json
 
-data = {'http://challenge.code2040.org/api/register' : 'https://github.com/DalidaMartinez/task1.git'    }
-request = Request("http://challenge.code2040.org/api/register")
 
-try:
-    response = urlopen(request, json.dump(data))
-    answer = response.read()
-    print (answer)
+def main():
 
-except URLError, e:
-    print ("error code", e)
+    token = 'c802285d1d9fcb3af4606c08279961ef'
+    gitURL = "https://github.com/DalidaMartinez/TasksCode2040"
+    req = "http://challenge.code2040.org/api/register"
+
+    dataInput = {'token' : token, 'github': gitURL  }
+
+    answer = requests.post(req, dataInput)
+
+
+    print answer
+
+main()
 
 
