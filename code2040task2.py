e2e10cb37566b81cd33b09acdefb88aa11ccf312
DalidaMartinez <dalidamartinez@gmail.com>
2016-11-01 16:11:15 -0400

    task #2 complete

--- code2040task2.py
+++ code2040task2.py
@@ -1,24 +1,21 @@
 import requests
-import sys
+
 
 def main():
-    token = 'c802285d1d9fcb3af4606c08279961ef'
     urlSend = "http://challenge.code2040.org/api/reverse"
     urlRetrieve = "http://challenge.code2040.org/api/reverse/validate"
 
-    tokenData= {'token' : token}
+    tokenData= {'token' : 'c802285d1d9fcb3af4606c08279961ef'}
 
     r = requests.post(urlSend, tokenData)
 
-    reversedString   = stringReverse(r.text)
+    reversedString   =  r.text[::-1]
 
-    data2 =  {'token' : tokenData, 'string': reversedString}
+    data2 =  {'token':'c802285d1d9fcb3af4606c08279961ef', 'string': reversedString}
 
     printStatement = requests.post(urlRetrieve, data2)
 
-    print printStatement
+    print printStatement.text
 
 main()
 
-def stringReverse(s):
-    return s[::-1]
